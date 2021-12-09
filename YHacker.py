e9d92aae910353e9c59439d62927c20af97f55ac
Zeron <kc.gita37@gmail.com>
2021-12-09 12:59:22 +0545

    Added few tweaks
     Added  magic touch

---
+++ YHacker.py
@@ -0,0 +1,28 @@
+import requests,csv
+from bs4 import  BeautifulSoup
+
+# for hi in range(1,5):	
+# 	url='https://news.ycombinator.com/news?p='+str(hi)
+# 	print(url)
+# 	Soup=BeautifulSoup(requests.get(url).text,'lxml')
+
+def news():
+	for hi in range(1,40):	
+		url='https://news.ycombinator.com/news?p='+str(hi)
+		Soup=BeautifulSoup(requests.get(url).text,'lxml')
+		hi=Soup.find_all(class_='athing')
+		for Box in hi:
+			Title=Box.find('a',class_='titlelink').text
+			link=Box.find('a',class_='titlelink')['href']
+			smallbox=Soup.find('td',class_='subtext')
+			score=smallbox.find('span',class_='score').text
+			age=smallbox.find('span',class_='age').text
+			countcomments=(list(smallbox.find_all('a'))[3].text).replace(' ',' ')
+			gtlnkkcoments=(list(smallbox.find_all('a'))[3])['href']
+			comments='https://news.ycombinator.com/'+str(gtlnkkcoments)
+			with open('Yhacker.csv',newline='',mode='a+') as fire:
+				spam=csv.writer(fire,delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+				spam.writerow([Title,link,score,age,countcomments,comments])
+
+news()
+
