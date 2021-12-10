7e080b0bd9820db9697faf8ad6fcbd3dbe82a1ab
Zeron <kc.gita37@gmail.com>
2021-12-10 09:43:37 +0545

    CHECKPOINT

--- YHacker.py
+++ YHacker.py
@@ -2,23 +2,38 @@ import requests,csv
 from bs4 import  BeautifulSoup
 
 def news():
-	for hi in range(1,5):	
+	for hi in range(1,3):	
 		url='https://news.ycombinator.com/news?p='+str(hi)
 		Soup=BeautifulSoup(requests.get(url).text,'lxml')
-		house=Soup.find_all(class_='athing')
-		for Box in house:
-			Title=Box.find('a',class_='titlelink').text
-			link=Box.find('a',class_='titlelink')['href']
-			smallbox=Soup.find('td',class_='subtext')
-			score=smallbox.find('span',class_='score').text
-			age=smallbox.find('span',class_='age').text
-			countcomments=(list(smallbox.find_all('a'))[3].text).replace(' ',' ')
-			gtlnkkcoments=(list(smallbox.find_all('a'))[3])['href']
-			comments='https://news.ycombinator.com/'+str(gtlnkkcoments)
-			print(Title)
-			# with open('YNews.csv',newline='',mode='a+') as fire:
-			# 	spam=csv.writer(fire,delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-			# 	spam.writerow([Title,link,score,age,countcomments,comments])
-
+		# House=Soup.find_all(class_='athing')
+		smallbox=Soup.find('td',class_='subtext')
+		
+		score=smallbox.find('span',class_='score').text
+		age=smallbox.find('span',class_='age').text
+		countcomments=(list(smallbox.find_all('a'))[3].text).replace(' ',' ')
+		gtlnkkcoments=(list(smallbox.find_all('a'))[3])['href']
+		comments='https://news.ycombinator.com/'+str(gtlnkkcoments)
+		
 news()
+
+
+# def hi():
+# 	for hi in range(1,3):	
+# 		url='https://news.ycombinator.com/news?p='+str(hi)
+# 		Soup=BeautifulSoup(requests.get(url).text,'lxml')
+# 		hi=Soup.find_all(class_='athing')
+# 		# for Box in hi:
+# 		Title=Box.find('a',class_='titlelink').text
+# 		link=Box.find('a',class_='titlelink')['href']
+# 		smallbox=Soup.find('td',class_='subtext')
+# 		score=smallbox.find('span',class_='score').text
+# 		age=smallbox.find('span',class_='age').text
+# 		countcomments=(list(smallbox.find_all('a'))[3].text).replace(' ',' ')
+# 		gtlnkkcoments=(list(smallbox.find_all('a'))[3])['href']
+# 		comments='https://news.ycombinator.com/'+str(gtlnkkcoments)
+# 		print(comments)
+# 		# with open('YNews.csv',newline='',mode='a+') as fire:
+# 		# 	spam=csv.writer(fire,delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+# 		# 	spam.writerow([Title,link,score,age,countcomments,comments])
+
 
