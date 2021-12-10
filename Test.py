561c7f5512696ef8c568cd6c5e8d6d6c6942a0e7
Zeron <kc.gita37@gmail.com>
2021-12-10 20:17:37 +0545

    fixing bug

--- Test.py
+++ Test.py
@@ -2,13 +2,39 @@ import requests,csv
 from bs4 import  BeautifulSoup
 
 
-url='https://news.ycombinator.com/news?p=1'
-Soup=BeautifulSoup(requests.get(url).text,'lxml')
+# url='https://news.ycombinator.com/news?p=1'
+# Soup=BeautifulSoup(requests.get(url).text,'lxml')
 
-texts=Soup.find_all('td',class_ ='subtext')
-for hi in texts:
-	score=hi.find('span',class_='score')
-	if score:
-		print(score.text)
-	elif score==None:
-		print('hi')
+# texts=Soup.find_all('td',class_ ='subtext')
+# for hi in texts:
+# 	score=hi.find('span',class_='score')
+# 	if score:
+# 		print(score.text)
+# 	elif score==None:
+# 		print('hi')
+def news():
+	for hi in range(1,3):	
+		url='https://news.ycombinator.com/news?p='+str(hi)
+		Soup=BeautifulSoup(requests.get(url).text,'lxml')
+		hi=Soup.find_all(class_='athing')
+		for Box in hi:
+			Points=[]
+			Title=Box.find('a',class_='titlelink').text
+			link=Box.find('a',class_='titlelink')['href']
+			Footer=Soup.find_all('td',class_='subtext')
+			for smallbox in Footer: 
+				scores=smallbox.find('span',class_='score')
+				Number=scores.text if score else "No data Found"
+				print(Number)
+
+
+				
+			age=smallbox.find('span',class_='age').text
+			countcomments=(list(smallbox.find_all('a'))[3].text).replace(' ',' ')
+			gtlnkkcoments=(list(smallbox.find_all('a'))[3])['href']
+			comments='https://news.ycombinator.com/'+str(gtlnkkcoments)
+			# with open('Yhacker.csv',newline='',mode='a+') as fire:
+			# 	spam=csv.writer(fire,delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+			# 	spam.writerow([Title,link,Points,age,countcomments,comments])
+
+news()
--- YHacker.py
+++ YHacker.py
@@ -6,8 +6,7 @@ def news():
 		url='https://news.ycombinator.com/news?p='+str(hi)
 		Soup=BeautifulSoup(requests.get(url).text,'lxml')
 		# House=Soup.find_all(class_='athing')
-		smallbox=Soup.find('td',class_='subtext')
-		
+		smallbox=Soup.find('td',class_='subtext')		
 		score=smallbox.find('span',class_='score').text
 		age=smallbox.find('span',class_='age').text
 		countcomments=(list(smallbox.find_all('a'))[3].text).replace(' ',' ')
@@ -15,25 +14,3 @@ def news():
 		comments='https://news.ycombinator.com/'+str(gtlnkkcoments)
 		
 news()
-
-
-# def hi():
-# 	for hi in range(1,3):	
-# 		url='https://news.ycombinator.com/news?p='+str(hi)
-# 		Soup=BeautifulSoup(requests.get(url).text,'lxml')
-# 		hi=Soup.find_all(class_='athing')
-# 		# for Box in hi:
-# 		Title=Box.find('a',class_='titlelink').text
-# 		link=Box.find('a',class_='titlelink')['href']
-# 		smallbox=Soup.find('td',class_='subtext')
-# 		score=smallbox.find('span',class_='score').text
-# 		age=smallbox.find('span',class_='age').text
-# 		countcomments=(list(smallbox.find_all('a'))[3].text).replace(' ',' ')
-# 		gtlnkkcoments=(list(smallbox.find_all('a'))[3])['href']
-# 		comments='https://news.ycombinator.com/'+str(gtlnkkcoments)
-# 		print(comments)
-# 		# with open('YNews.csv',newline='',mode='a+') as fire:
-# 		# 	spam=csv.writer(fire,delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-# 		# 	spam.writerow([Title,link,score,age,countcomments,comments])
-
-
